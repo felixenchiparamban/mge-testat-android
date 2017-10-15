ada21e377607e55000b7624890bbd1596fd678f4
felix <felixenchiparambil@hotmail.com>
2017-10-15 15:50:36 +0200

    home and loans view implementation

--- app/src/main/java/ch/hsr/mge/gadgeothek/HomeActivity.java
+++ app/src/main/java/ch/hsr/mge/gadgeothek/HomeActivity.java
@@ -3,22 +3,23 @@ package ch.hsr.mge.gadgeothek;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
 
-    private Button btnLoans;
-    private Button btnReserve;
+    private View btnLoans;
+    private View btnReserve;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        btnLoans = (Button)findViewById(R.id.btnLoan);
-        btnReserve = (Button)findViewById(R.id.btnReservation);
+        btnLoans = findViewById(R.id.loansCard);
+        btnReserve = findViewById(R.id.reservationsCard);
 
         btnLoans.setOnClickListener(new View.OnClickListener(){
              @Override
