c0382e88299b7e6693f83a196b12f31bf9e9c618
felix <felixenchiparambil@hotmail.com>
2017-10-21 14:13:51 +0200

    added reservation implementation

--- app/src/main/java/ch/hsr/mge/gadgeothek/ReservationActivity.java
+++ app/src/main/java/ch/hsr/mge/gadgeothek/ReservationActivity.java
@@ -2,10 +2,22 @@ package ch.hsr.mge.gadgeothek;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 
-import ch.hsr.mge.gadgeothek.R;
+import java.util.List;
+
+import ch.hsr.mge.gadgeothek.adapter.ReservationAdapter;
+import ch.hsr.mge.gadgeothek.domain.Reservation;
+import ch.hsr.mge.gadgeothek.service.Callback;
+import ch.hsr.mge.gadgeothek.service.LibraryService;
 
 public class ReservationActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private RecyclerView.LayoutManager layoutManager;
+    private ReservationAdapter reservationAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +25,30 @@ public class ReservationActivity extends AppCompatActivity {
         setContentView(R.layout.activity_reservation);
 
         getSupportActionBar().setTitle("Reservation");
+
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerViewReservation);
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        reservationAdapter = new ReservationAdapter();
+        recyclerView.setAdapter(reservationAdapter);
+
+        fetchReservation();
+    }
+
+    private void fetchReservation() {
+        LibraryService.getReservationsForCustomer(new Callback<List<Reservation>>() {
+            @Override
+            public void onCompletion(List<Reservation> input) {
+                reservationAdapter.setReservations(input);
+                reservationAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onError(String message) {
+                Log.d("Error fetchReservation:", message);
+            }
+        });
     }
 }
